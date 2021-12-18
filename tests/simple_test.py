da6d858f0e15d0c9e7c0612efa66653ada7e1375
Ed Izotov <ed.izotov@gmail.com>
2021-12-18 12:28:45 +0200

    Add class

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
 
 class TestClass:
     def test_something(self):
         driver = webdriver.Chrome(executable_path='../../chromedriver.exe')
         driver.get("http://151.80.70.42:3000/")
-        create_button = driver.find_element_by_css_selector('i.mdi-content-add')
+        create_button = driver.find_element(By.CSS_SELECTOR, 'i.mdi-content-add')
         create_button.click()
         name = driver.find_element_by_css_selector('div.main-content input#icon_prefix')
         name.clear()
