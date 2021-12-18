d6663c9a21b9c7d7a6d74ae5f1725049aae427ab
Ed Izotov <ed.izotov@gmail.com>
2021-12-18 12:26:35 +0200

    Add class

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -1,1 +1,17 @@
-assert 1 == 2
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class TestClass:
+    def test_something(self):
+        driver = webdriver.Chrome(executable_path='../../chromedriver.exe')
+        driver.get("http://151.80.70.42:3000/")
+        create_button = driver.find_element_by_css_selector('i.mdi-content-add')
+        create_button.click()
+        name = driver.find_element_by_css_selector('div.main-content input#icon_prefix')
+        name.clear()
+        name.send_keys('VASYAAAAA')
+        phone = driver.find_element_by_css_selector('div.main-content input#icon_telephone')
+        phone.clear()
+        phone.send_keys('1234567899')
+        driver.find_element_by_css_selector('div.main-content a.save-btn').click()
+        driver.close()
